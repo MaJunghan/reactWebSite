7e14b6356eaeee00bf03993b7d3b39edb1f8d968
MaJunghan <wjdgksak@naver.com>
2022-07-11 18:07:36 +0900

    임시

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,14 +6,7 @@ const About = () => {
   return (
     <section id="about" className="section section__container">
       <h1>About me</h1>
-      <p>
-        LG 차세대커머스에서 nuxt.js로 Mobile 관련 페이지를 6개월 정도 제작한
-        경험있습니다.
-        <br />
-        최근에는 vue.js 뿐만 아니라 react.js에도 관심이 생겨서 공부하고있고,
-        <br />
-        해당 내용들은 블로그에서 확인 할 수 있습니다.
-      </p>
+      <p>1</p>
       <div className="about__majors">
         <div className="major">
           <div className="major__icon">
